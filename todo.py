711f2d3829315364dcd9bd8cc2422df5d73a6a62
Ankit Singh <ankitsingh123.as17@gmail.com>
2025-04-21 10:54:31 +0530

    file reading feature added, gonna use this txt file to store the tasks

--- todo.py
+++ todo.py
@@ -5,6 +5,12 @@ root=tk.Tk()
 root.title("To-do list")
 
 
+def add_task():
+    print("task added")
+
+def del_task():
+    print("task deleted")
+
 label=tk.Label(root,text="Enter the task",font=('Arial',24))
 label.pack()
 
@@ -13,12 +19,20 @@ entry_task.pack(pady=10)
 
 
 btn_add=tk.Button(root,text="add task",width=48,command=add_task)
-btn_add.pack()
+btn_add.pack(pady=10)
 
+btn_del=tk.Button(root,text="Delete button",width=48,command=del_task)
+btn_del.pack(pady=10)
 
+list_Task=tk.Listbox(root,selectmode=tk.SINGLE,height=10,width=50)
+list_Task.pack(pady=20)
 
-
-
-
+try:
+    with open("tasks.txt", "r") as file:
+        tasks=file.read().splitlines()
+    for task in tasks:
+        list_Task.insert(tk.END,task)
+except FileExistsError:
+    print("File not found")
 
 root.mainloop()
