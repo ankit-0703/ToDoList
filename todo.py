d943db1ec33195e11bb38d7fc6d0fc93a4d7f935
Ankit Singh <ankitsingh123.as17@gmail.com>
2025-04-21 10:39:12 +0530

    button and label is added

--- todo.py
+++ todo.py
@@ -4,4 +4,21 @@ from tkinter import messagebox
 root=tk.Tk()
 root.title("To-do list")
 
+
+label=tk.Label(root,text="Enter the task",font=('Arial',24))
+label.pack()
+
+entry_task=tk.Entry(root,width=50)
+entry_task.pack(pady=10)
+
+
+btn_add=tk.Button(root,text="add task",width=48,command=add_task)
+btn_add.pack()
+
+
+
+
+
+
+
 root.mainloop()
